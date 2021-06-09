6027c0c2933d15711426a15aab992c6a68a2cfad
Sebastian Schumacher <sebastian.schumacher@e-pixler.com>
2021-06-09 11:30:00 +0200

    general setup and header

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="
+      bg-normal-black
+      items-center
+      w-screen
+      flex
+      justify-between
+      text-white
+      p-3
+    "
+  >
+    <p>Benedikt Stoll</p>
+    <div class="tags flex w-7/12 justify-between">
+      <p>Kritische Raumproduktion</p>
+      <p>Spekulative Forschung</p>
+      <p>Performativer Urbanismus</p>
+    </div>
+  </div>
+</template>
+
+<style>
+.tags * {
+  padding: 0 10px;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'normal-black': '#222222',
+        'light-blue': '#017EB3',
+        'soft-blue': '#ecf8fd',
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  },
+}
