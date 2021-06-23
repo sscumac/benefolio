b53a0fd43a27ec2fc8b57849e796b698c58d475c
sscumac <sebastian.schumacher@hotmail.de>
2021-06-23 15:44:32 +0200

    added bottom header

--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,40 @@
 <template>
-  <div
-    class="
-      bg-normal-black
-      items-center
-      w-screen
-      flex
-      justify-between
-      text-white
-      p-3
-    "
-  >
-    <p>Benedikt Stoll</p>
-    <div class="tags flex w-7/12 justify-between">
-      <p>Kritische Raumproduktion</p>
-      <p>Spekulative Forschung</p>
-      <p>Performativer Urbanismus</p>
+  <div>
+    <!--  top-->
+    <div
+      class="
+        bg-normal-black
+        items-center
+        w-screen
+        flex
+        justify-between
+        text-white
+        p-3
+      "
+    >
+      <p>Benedikt Stoll</p>
+      <div class="tags flex w-7/12 justify-between">
+        <p>Kritische Raumproduktion</p>
+        <p>Spekulative Forschung</p>
+        <p>Performativer Urbanismus</p>
+      </div>
+    </div>
+    <!--    bottom-->
+    <div
+      class="
+        bg-light-blue
+        items-center
+        w-screen
+        flex
+        justify-end
+        text-white
+        p-3
+        tags
+      "
+    >
+      <nuxt-link class="tags" to="/">Portfolio</nuxt-link>
+      <nuxt-link class="tags" to="/">Info</nuxt-link>
+      <nuxt-link class="tags" to="/">ENG</nuxt-link>
     </div>
   </div>
 </template>
