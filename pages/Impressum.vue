88505a4733617f8684c0b839cab6278fb3e29867
Sebastian Schumacher <sebastian.schumacher@e-pixler.com>
2021-08-10 20:42:06 +0200

    impressum complete

--- pages/Impressum.vue
+++ pages/Impressum.vue
@@ -1,28 +1,86 @@
 <template>
   <div class="container flex max-w-full">
-    <div class="flex">
-      <div class="mx-auto"></div>
+    <div class="flex w-full">
+      <div class="mx-auto" />
       <div class="w-1/2 flex flex-col lg:ml-20">
-        <h1 class="font-bold mb-4">Impressum_beta</h1>
+        <h1 class="font-bold mb-4">Impressum</h1>
         <p>
-          El moluptat qui aspist ut fugias nemquo maio. Neque quo mosae nobist
-          auditatio eliquam quia quia doluptate volles- tiam, conserem. Ut utem
-          quunt, niscimus as doluptu scient adipsam, cones ratecto rruptatem. Ut
-          quaerferum et rep- ersp erfereptatur aciunt quiatem re quatist, sunt
-          aut et abo. Et is aut reni is molorro vidigendi ipsam, nim
-          verovidemque is evel molupta cori omnienimet omnihil ipsam re, atati
-          dis res con pliquo beratum quiam dolores et erum qui similitat
-          occullectas earuptatas alic tet eaquos qui omnis ea quam ipicips
-          antectur adit postibusciis eos net occumqu undebis dolupti squaspe
-          riandaero molorum essequid maxim quia sum facepelit eos nesedis atur,
-          verum, omnim rero eum id magnatem aut laborib ernate autecto volese
-          numet ipsunt
+          Herausgeber und Autor der Website: Benedikt Stoll <br />
+          Code: Sebastian Schumacher
         </p>
-        <div class="flex mt-10">
-          <div class="font-bold mr-2">Kontakt</div>
-          <a class="class" href="mailto:mail@studio.eu">mail@studio.eu</a>
-        </div>
+        <p>
+          Der Autor übernimmt keinerlei Gewähr für die Aktualität, Korrektheit,
+          Vollständigkeit oder Qualität der bereitgestellten Informationen.
+          Haftungsansprüche gegen den Autor, die sich auf Schäden materieller
+          oder ideeller Art beziehen, welche durch die Nutzung oder Nichtnutzung
+          der dargebotenen Informationen bzw. durch die Nutzung fehlerhafter und
+          unvollständiger Informationen verursacht wurden sind grundsätzlich
+          ausgeschlossen, sofern seitens des Autors kein nachweislich
+          vorsätzliches oder grob fahrlässiges Verschulden vorliegt.
+        </p>
+        <p>
+          Alle Angebote sind freibleibend und unverbindlich. Der Autor behält es
+          sich ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne
+          gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder die
+          Veröffentlichung zeitweise oder endgültig einzustellen.
+        </p>
+        <h6>Verweise und Links</h6>
+        <p>
+          Bei direkten oder indirekten Verweisen auf fremde Internetseiten
+          (“Links”), die außerhalb des Verantwortungsbereiches des Autors
+          liegen, haftet dieser nur dann, wenn er von den Inhalten Kenntnis hat
+          und es ihm technisch möglich und zumutbar wäre, die Nutzung im Falle
+          rechtswidriger Inhalte zu verhindern. Der Autor erklärt hiermit
+          ausdrücklich, dass zum Zeitpunkt der Linksetzung die entsprech enden
+          verlinkten Seiten frei von illegalen Inhalten waren. Der Autor erklärt
+          weiterhin, dass er keinerlei Einfluss auf die aktuelle und zukünftige
+          Gestaltung und auf die Inhalte der gelinkten/verknüpften Seiten hat.
+          Deshalb distanziert er sich hiermit ausdrücklich von allen Inhalten
+          aller gelinkten /verknüpften Seiten, die nach der Linksetzung
+          verändert wurden. Diese Feststellung gilt für alle innerhalb des
+          eigenen Internetangebotes gesetzten Links und Verweise sowie für
+          Fremdeinträge in vom Autor eingerichteten Gästebüchern,
+          Diskussionsforen und Mailinglisten. Für illegale, fehlerhafte oder
+          unvollständige Inhalte und insbesondere für Schäden, die aus der
+          Nutzung oder Nichtnutzung solcherart dargebotener Informationen
+          entstehen, haftet allein der Anbieter der Seite, auf welche verwiesen
+          wurde, nicht derjenige, der über Links auf die jeweilige Veröffentli
+          chung lediglich verweist.
+        </p>
+        <h6>Urheberrecht</h6>
+        <p>
+          Der Autor ist bestrebt, in allen Publikationen die Urheberrechte der
+          verwendeten Grafiken, Tondokumente, Videosequenzen und Texte zu
+          beachten, von ihm selbst erstellte Grafiken, Tondokumente,
+          Videosequenzen und Texte zu nutzen oder auf lizenzfreie Grafiken,
+          Tondokumente, Videosequenzen und Texte zurückzugreifen.
+        </p>
+        <p>
+          Das Copyright für veröffentlichte, vom Autor selbst erstellte Objekte
+          bleibt allein beim Autor der Seiten. Eine Vervielfältigung oder
+          Verwendung solcher Grafiken, Tondokumente, Videosequenzen und Texte in
+          anderen elektronischen oder gedruckten Publikationen ist ohne
+          ausdrückliche Zustimmung des Autors nicht gestattet.
+        </p>
+        <h6>Rechtswirksamkeit dieses Haftungsausschlusses</h6>
+        <p>
+          Dieser Haftungsausschluss ist als Teil des Internetangebotes zu
+          betrachten, von dem aus auf diese Seite verwiesen wurde. Sofern Teile
+          oder einzelne Formulierungen dieses Textes der geltenden Rechtslage
+          nicht, nicht mehr oder nicht vollständig entsprechen sollten, bleiben
+          die übrigen Teile des Dokumentes in ihrem Inhalt und ihrer Gültigkeit
+          davon unberührt.
+        </p>
+        <p>Angaben gemäß Teledienstgesetz</p>
+        <p>Benedikt Stoll</p>
+
+        <a class="class" href="mailto:mail@studio.eu">mail@studio.eu</a>
       </div>
     </div>
   </div>
 </template>
+<style>
+p {
+  margin-top: 1rem;
+}
+</style>
