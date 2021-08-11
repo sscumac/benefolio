b437b1471858ba4232595961253b42744a0dad77
sschumac <sebastian.schumacher@hotmail.de>
2021-08-11 12:39:45 +0200

    datenschutz page complete

--- pages/Datenschutz.vue
+++ pages/Datenschutz.vue
@@ -1,5 +1,182 @@
 <template>
-  <div class="container">
-    <p class="m-10">Datenschutz</p>
+  <div class="container flex max-w-full">
+    <div class="flex w-full">
+      <div class="hidden mx-auto lg:block" />
+      <div class="lg:w-1/2 flex flex-col lg:ml-20">
+        <h1 class="font-bold mb-4">Datenschutz</h1>
+        <p>
+          Name und Kontakt des Verantwortlichen gemäß Artikel 4 Abs. 7 DSGVO
+        </p>
+        <p>
+          Benedikt Stoll <br />
+          Berlin <br />
+          <a class="class" href="mailto:mail@studio.eu">mail@studio.eu</a>
+        </p>
+        <h6>Sicherheit und Schutz Ihrer personenbezogenen Daten</h6>
+        <p>
+          Wir betrachten es als unsere vorrangige Aufgabe, die Ver- traulichkeit
+          der von Ihnen bereitgestellten personenbezo- genen Daten zu wahren und
+          diese vor unbefugten Zugriff- en zu schützen. Deshalb wenden wir
+          äußerste Sorgfalt und modernste Sicherheitsstandards an, um einen
+          maximalen Schutz Ihrer personenbezogenen Daten zu gewährleisten.
+        </p>
+        <h6>Verweise und Links</h6>
+        <p>
+          Als privatrechtliches Unternehmen unterliegen wir den Bestimmungen der
+          europäischen Datenschutzgrundver- ordnung (DSGVO) und den Regelungen
+          des Bundesdat- enschutzgesetzes (BDSG). Wir haben technische und or-
+          ganisatorische Maßnahmen getroffen, die sicherstellen, dass die
+          Vorschriften über den Datenschutz sowohl von uns, als auch von unseren
+          externen Dienstleistern beachtet werden.
+        </p>
+        <p>
+          Der Autor ist bestrebt, in allen Publikationen die Urheberrechte der
+          verwendeten Grafiken, Tondokumente, Videosequenzen und Texte zu
+          beachten, von ihm selbst erstellte Grafiken, Tondokumente,
+          Videosequenzen und Texte zu nutzen oder auf lizenzfreie Grafiken,
+          Tondokumente, Videosequenzen und Texte zurückzugreifen.
+        </p>
+        <h6>Begriffsbestimmungen</h6>
+        <p>
+          Der Gesetzgeber fordert, dass personenbezogene Daten auf rechtmäßige
+          Weise, nach Treu und Glauben und in einer für die betroffene Person
+          nachvollziehbaren Weise verar- beitet werden („Rechtmäßigkeit,
+          Verarbeitung nach Treu und Glauben, Transparenz“). Um dies zu
+          gewährleisten, in- formieren wir Sie über die einzelnen gesetzlichen
+          Begriffs- bestimmungen, die auch in dieser Datenschutzerklärung
+          verwendet werden:
+        </p>
+        <p>
+          Personenbezogene Daten <br />
+          „Personenbezogene Daten“ sind alle Informationen, die sich auf eine
+          identifizierte oder identifizierbare natürliche Person (im Folgenden
+          „betroffene Person“) beziehen; als identifizierbar wird eine
+          natürliche Person angesehen, die direkt oder indirekt, insbesondere
+          mittels Zuordnung zu einer Kennung wie einem Namen, zu einer
+          Kennnummer, zu Standortdaten, zu einer Online-Kennung oder zu einem
+          oder mehreren besonderen Merkmalen identifiziert werden kann, die
+          Ausdruck der physischen, physiologischen, ge- netischen, psychischen,
+          wirtschaftlichen, kulturellen oder sozialen Identität dieser
+          natürlichen Person sind. <br />
+
+          Verarbeitung <br />
+          „Verarbeitung“ ist jeder, mit oder ohne Hilfe automatisi- erter
+          Verfahren, ausgeführter Vorgang oder jede solche Vorgangsreihe im
+          Zusammenhang mit personenbezogenen Daten wie das Erheben, das
+          Erfassen, die Organisation, das Ordnen, die Speicherung, die Anpassung
+          oder Veränder- ung, das Auslesen, das Abfragen, die Verwendung, die
+          Offenlegung durch Übermittlung, Verbreitung oder eine andere Form der
+          Bereitstellung, den Abgleich oder die Verknüpfung, die Einschränkung,
+          das Löschen oder die Vernichtung. <br />
+
+          Einschränkung der Verarbeitung <br />
+          „Einschränkung der Verarbeitung“ ist die Markierung gespeicherter
+          personenbezogener Daten mit dem Ziel, ihre künftige Verarbeitung
+          einzuschränken. <br />
+
+          Profiling <br />
+          „Profiling“ ist jede Art der automatisierten Verarbeitung
+          personenbezogener Daten, die darin besteht, dass diese
+          personenbezogenen Daten verwendet werden, um bestim- mte persönliche
+          Aspekte, die sich auf eine natürliche Per- son beziehen, zu bewerten,
+          insbesondere um Aspekte bezüglich Arbeitsleistung, wirtschaftliche
+          Lage, Gesund- heit, persönliche Vorlieben, Interessen,
+          Zuverlässigkeit, Verhalten, Aufenthaltsort oder Ortswechsel dieser
+          natürli- chen Person zu analysieren oder vorherzusagen. <br />
+
+          Pseudonymisierung <br />
+          „Pseudonymisierung“ ist die Verarbeitung personenbezo- gener Daten in
+          einer Weise, dass die personenbezogenen Daten ohne Hinzuziehung
+          zusätzlicher Informationen nicht mehr einer spezifischen betroffenen
+          Person zugeordnet werden können, sofern diese zusätzlichen
+          Informationen gesondert aufbewahrt werden und technischen und organ-
+          isatorischen Maßnahmen unterliegen, die gewährleisten, dass die
+          personenbezogenen Daten nicht einer identifi- zierten oder
+          identifizierbaren natürlichen Person zugew- iesen werden können.
+          <br />
+
+          Dateisystem <br />
+          „Dateisystem“ ist jede strukturierte Sammlung personen- bezogener
+          Daten, die nach bestimmten Kriterien zugäng- lich sind, unabhängig
+          davon, ob diese Sammlung zentral, dezentral oder nach funktionalen
+          oder geografischen Gesi- chtspunkten geordnet geführt wird. <br />
+
+          Verantwortlicher <br />
+          „Verantwortlicher“ ist eine natürliche oder juristische Person,
+          Behörde, Einrichtung oder andere Stelle, die allein oder gemeinsam mit
+          anderen über die Zwecke und Mittel der Verarbeitung von
+          personenbezogenen Daten entsc- heidet; sind die Zwecke und Mittel
+          dieser Verarbeitung durch das Unionsrecht oder das Recht der
+          Mitgliedstaaten vorgegeben, so können der Verantwortliche beziehung-
+          sweise die bestimmten Kriterien seiner Benennung nach dem Unionsrecht
+          oder dem Recht der Mitgliedstaaten vorgesehen werden. <br />
+
+          Auftragsverarbeiter <br />
+          „Auftragsverarbeiter“ ist eine natürliche oder juristische Person,
+          Behörde, Einrichtung oder andere Stelle, die per- sonenbezogene Daten
+          im Auftrag des Verantwortlichen verarbeitet. <br />
+          Empfänger <br />
+          „Empfänger“ ist eine natürliche oder juristische Person, Behörde,
+          Einrichtung oder andere Stelle, denen personen- bezogene Daten
+          offengelegt werden, unabhängig davon, ob es sich bei ihr um einen
+          Dritten handelt oder nicht. Be- hörden, die im Rahmen eines bestimmten
+          Untersuchun- gsauftrags nach dem Unionsrecht oder dem Recht der
+          Mitgliedstaaten möglicherweise personenbezogene Daten erhalten, gelten
+          jedoch nicht als Empfänger; die Verarbei- tung dieser Daten durch die
+          genannten Behörden erfolgt im Einklang mit den geltenden
+          Datenschutzvorschriften gemäß den Zwecken der Verarbeitung. <br />
+
+          Dritter <br />
+          „Dritter“ ist eine natürliche oder juristische Person, Be- hörde,
+          Einrichtung oder andere Stelle, außer der betrof- fenen Person, dem
+          Verantwortlichen, dem Auftragsver- arbeiter und den Personen, die
+          unter der unmittelbaren Verantwortung des Verantwortlichen oder des
+          Auftrags- verarbeiters befugt sind, die personenbezogenen Daten zu
+          verarbeiten. <br />
+
+          Einwilligung <br />
+          Eine „Einwilligung“ der betroffenen Person ist jede frei- willig für
+          den bestimmten Fall, in informierter Weise und unmissverständlich
+          abgegebene Willensbekundung in Form einer Erklärung oder einer
+          sonstigen eindeutigen bestäti- genden Handlung, mit der die betroffene
+          Person zu verste- hen gibt, dass sie mit der Verarbeitung der sie
+          betreffend- en personenbezogenen Daten einverstanden ist.
+        </p>
+
+        <h6>Rechtmäßigkeit der Verarbeitung</h6>
+        <p>
+          Die Verarbeitung personenbezogener Daten ist nur rech- tmäßig, wenn
+          für die Verarbeitung eine Rechtsgrundlage besteht. Rechtsgrundlage für
+          die Verarbeitung können gemäß Artikel 6 Abs. 1 lit. a – f DSGVO
+          insbesondere sein:
+        </p>
+        <p>
+          Die betroffene Person hat ihre Einwilligung zu der Verarbeitung der
+          sie betreffenden personenbezogenen Daten für einen oder mehrere
+          bestimmte Zwecke gegeben; die Verarbeitung ist für die Erfüllung eines
+          Vertrags, dessen Vertragspartei die betroffene Person ist, oder zur
+          Durchführung vorvertraglicher Maßnahmen erforderlich, die auf Anfrage
+          der betroffenen Person erfolgen; die Verarbeitung ist zur Erfüllung
+          einer rechtlichen Verpflichtung erforderlich, der der Verantwortliche
+          unterliegt; die Verarbeitung ist erforderlich, um lebenswichtige
+          Interessen der betroffenen Person oder einer anderen natürlichen
+          Person zu schützen; die Verarbeitung ist für die Wahrnehmung einer
+          Aufgabe erforderlich, die im öffentlichen Interesse liegt oder in
+          Ausübung öffentlicher Gewalt erfolgt, die dem Verantwortlichen
+          übertragen wurde; die Verarbeitung ist zur Wahrung der berechtigten
+          Interessen des Verantwortlichen oder eines Dritten erforderlich,
+          sofern nicht die Interessen oder Grundrechte und Grundfreiheiten der
+          betroffenen Person, die den Schutz personenbezogener Daten erfordern,
+          überwiegen, insbesondere dann, wenn es sich bei der betroffenen Person
+          um ein Kind handelt.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+<style>
+p,
+h6 {
+  margin-top: 1rem;
+}
+</style>
