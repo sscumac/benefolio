0ca1a6bb12d839439b7f5ac14b8fb8d086106420
sscumac <sebastian.schumacher@hotmnail.de>
2022-05-10 11:30:03 +0200

    added google analytics module / SEO improvements

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,14 +14,18 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'benefolio',
+    title: 'Benedikt Stoll',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'de',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'KUNST IM ÖFFENTLICHEN RAUM | SPEKULATIVE FORSCHUNG',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
@@ -41,6 +45,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-analytics',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -48,6 +53,10 @@ export default {
 
   generate: {
     routes: dynamicRoutes,
+  },
+
+  googleAnalytics: {
+    id: 'G-YX3QNQPX69',
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
